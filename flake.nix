c3100bbc390280f0660cfae0fa44bd742155be81
bodenlosus <johannes.till.schmidt@outlook.de>
2024-12-11 08:20:07 +0100

    updated to beta 1.0.2-b0

--- flake.nix
+++ flake.nix
@@ -6,19 +6,19 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      version = "1.0.1-a.22";
+      version = "1.0.2-b.0";
       downloadUrl = {
         "specific" = {
           url =
             "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
           sha256 =
-            "sha256:0anr79rdm62h5y37xa47rrrk32r9gnv04df4z7brc0hp4q83pxvi";
+            "sha256:067m7g48nfa366ajn3flphnwkx8msc034r6px8ml66mbj7awjw4x";
         };
         "generic" = {
           url =
             "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-generic.tar.bz2";
           sha256 =
-            "sha256:065rl1fhg79bkj1qy960qcid7wr7vd7j3wsf7bbr69b4rgmqqv3z";
+            "sha256:02x4w2fq80s1za05s0gg9r2drr845bln80h5hbwhvp1gxq9jf0g2";
         };
       };
 
