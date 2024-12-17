dd146732f1ec36f824251682b401e9a87d032c5b
ProggerX <x@proggers.ru>
2024-12-17 20:37:40 +0300

    Update to 1.0.2-b.2

--- flake.nix
+++ flake.nix
@@ -9,11 +9,11 @@
 		{ self, nixpkgs }:
 		let
 			system = "x86_64-linux";
-			version = "1.0.2-b.1";
+			version = "1.0.2-b.2";
 			downloadUrl = {
 				"specific" = {
 					url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
-					sha256 = "sha256:1bnalbpzk6alihjsvl9nmzn7zfy9a3dcil8dbrlbfz68jiz88hl1";
+					sha256 = "sha256:1dv98hg48g3kg2d0hxxg5jyf8yfdn3s7smbwii26vrpbigwp3ch7";
 				};
 			};
 
