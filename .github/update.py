a7ccb2a4ee45ace6662d6741b9c615055d02ac0b
bodenlosus <johannes.till.schmidt@outlook.de>
2024-12-20 13:17:55 +0100

    added automatisation

--- .github/update.py
+++ .github/update.py
@@ -2,18 +2,14 @@ from version import isVersionUpToDate, getNewVersion, updateVersion
 from generateFlake import generateFlake
 def update():
     newVersion, publishedAt = getNewVersion()
-    isUpToDate = isVersionUpToDate(newVersion)
     
-    if isUpToDate:
-        print(f"Zen Browser is up to date. Current version: {newVersion}")
-        return
-    
-    print(f"New version available: {newVersion} ({publishedAt})")
+    print(f"Updating to: {newVersion} ({publishedAt})")
     
     # UPDATE FLAKE
+    print("generating flake.nix file...")
     generateFlake(version=newVersion)
     
-    updateVersion(newVersion)
+    print("updated flake.nix file")
     
 if __name__ == "__main__":
     update()
