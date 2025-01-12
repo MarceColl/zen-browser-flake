89a8fee4bb6fe48212fbbd2aa47b620a4d6f1e2d
Surya Vamsi <d.suryavamsi@gmail.com>
2025-01-12 15:13:20 +0530

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -8,15 +8,15 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      version = "1.0.1-a.6";
+      version = "1.0.2-b.5";
       downloadUrl = {
         "specific" = {
-	  url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
-	  sha256 = "sha256:0jkzdrsd1qdw3pwdafnl5xb061vryxzgwmvp1a6ghdwgl2dm2fcz";
+	  url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.bz2";
+	  sha256 = "2b7cd308b76f83f55840db1f7a8870eb91a193c1408e13a5f215d4e9110ee3fb";
 	};
 	"generic" = {
-	  url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-generic.tar.bz2";
-	  sha256 = "sha256:17c1ayxjdn8c28c5xvj3f94zjyiiwn8fihm3nq440b9dhkg01qcz";
+	  url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.bz2";
+	  sha256 = "2b7cd308b76f83f55840db1f7a8870eb91a193c1408e13a5f215d4e9110ee3fb";
 	};
       };
 
